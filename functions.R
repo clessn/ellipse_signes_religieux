c9adfdfce1d65a78bad34a577ec64829681ee701
sarahjanevincent <141277350+sarahjanevincent@users.noreply.github.com>
2024-03-26 09:33:46 -0400

    function+2019

--- functions.R
+++ functions.R
@@ -1,6 +1,6 @@
-clean_var <- function(raw_vector, target = c("teacher", "autority")){
+clean_var <- function(raw_vector, target = c("teacher", "authority")){
   patterns <- c("teacher" = "public school teachers",
-                "autority" = "government officials")
+                "authority" = "government officials")
   pattern <- patterns[target]
   output <- as.numeric(grepl(pattern = pattern, x = raw_vector))
   return(output)
