102711615f472851a1154fac1bb8eebfe282c592
sarahjanevincent <141277350+sarahjanevincent@users.noreply.github.com>
2024-03-26 09:37:47 -0400

    fini2019

--- cleaning/cleaning2019.R
+++ cleaning/cleaning2019.R
@@ -5,73 +5,23 @@ source("functions.R")
 # Data --------------------------------------------------------------------
 
 raw_df <- read.csv("SignesReligieux2024/Data/data_pes_canada2019_weights.csv") %>%
-  ### filter for Quebec
-  filter(Q3 == "Quebec" | Q4 == "Quebec")
-
-clean_df <- data.frame(
-  id = 1:nrow(raw_df),
-  year = 2019,
-  weight = as.numeric(raw_df$weight)
-)
+  filter(Q3 == "Quebec" | Q4 == "Quebec") %>%
+  select(Q190, Q192, Q196, Q187, weight)
 
 # Clean -------------------------------------------------------------------
+clean_df <- tidyr::pivot_longer(raw_df, cols = c(Q190, Q192, Q196, Q187),
+                                names_to = "symbol", values_to = "value") %>%
 
-## This symbol should be banned in the following cases (select all that apply):
-## For government officials in positions of authority (e.g. judges, police) (13)
-## For participants in the Canadian citizenship ceremony (14)
-## For public school teachers (15)
-## For public school students (16)
-## For those in all public spaces (e.g. on sidewalks, in public parks) (17)
-## In none of these cases (18)
-
-## 4 Symboles
-## Hijab: Q190
-## Pendantif croissant: Q192
-## Petite croix: Q196
-## Grosse croix: Q187
-
-### Hijab ####
-hijab <- raw_df$Q190
-table(hijab)
-clean_df$autority_hijab <- clean_var(raw_vector = hijab,
-                                     target = "autority")
-table(clean_df$autority_hijab)
-
-clean_df$teacher_hijab <- clean_var(raw_vector = hijab,
-                                    target = "teacher")
-table(clean_df$teacher_hijab)
-
-### Pendantif croissant ####
-pendantif_croissant <- raw_df$Q192
-table(pendantif_croissant)
-clean_df$autority_pendantif_croissant <- clean_var(raw_vector = pendantif_croissant,
-                                                   target = "autority")
-table(clean_df$autority_pendantif_croissant)
-
-clean_df$teacher_pendantif_croissant <- clean_var(raw_vector = pendantif_croissant,
-                                                  target = "teacher")
-table(clean_df$teacher_pendantif_croissant)
-
-### Petite croix ####
-petite_croix <- raw_df$Q196
-table(petite_croix)
-clean_df$autority_petite_croix <- clean_var(raw_vector = petite_croix,
-                                            target = "autority")
-table(clean_df$autority_petite_croix)
-
-clean_df$teacher_petite_croix <- clean_var(raw_vector = petite_croix,
-                                           target = "teacher")
-table(clean_df$teacher_petite_croix)
-
-### Grosse croix ####
-grosse_croix <- raw_df$Q187
-table(grosse_croix)
-clean_df$autority_grosse_croix <- clean_var(raw_vector = grosse_croix,
-                                            target = "autority")
-table(clean_df$autority_grosse_croix)
-
-clean_df$teacher_grosse_croix <- clean_var(raw_vector = grosse_croix,
-                                           target = "teacher")
-table(clean_df$teacher_grosse_croix)
+  tidyr::drop_na(value) %>%
+  mutate(symbol = case_when(
+    symbol == "Q190" ~ "hijab",
+    symbol == "Q192" ~ "pendantif_croissant",
+    symbol == "Q196" ~ "petite_croix",
+    symbol == "Q187" ~ "grosse_croix"
+  ),
+  authority = clean_var(value, target = "authority"),
+  teacher = clean_var(value, target = "teacher"),
+  year = 2019) %>%
+  select(year, symbol, authority, teacher, weight)
 
 saveRDS(clean_df, "SignesReligieux2024/Data/cleandata/by_year/data2019.rds")
