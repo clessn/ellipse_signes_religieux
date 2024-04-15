f28d77357451856750314bf96507e95ed149af18
hubcad25 <hubertcadieux@gmail.com>
2024-04-15 16:08:07 -0400

    add weight for 2022

--- cleaning/cleaning2022.R
+++ cleaning/cleaning2022.R
@@ -10,9 +10,10 @@ raw_df <- read.csv("SignesReligieux2024/Data/pes_qc22_relig_symb/2022-11-09_pes_
          age_weight, gender_weight, language_weight) %>%
   mutate(id_respondent = 1:nrow(.))
 
-# Clean -------------------------------------------------------------------
+# Symbols -------------------------------------------------------------------
 
-clean_df <- tidyr::pivot_longer(raw_df, cols = c(Q101, Q103, Q107, Q98),
+df_symbols <- tidyr::pivot_longer(raw_df %>% select(id_respondent, Q101, Q103, Q107, Q98),
+                                  cols = c(Q101, Q103, Q107, Q98),
                                 names_to = "symbol", values_to = "value") %>%
   tidyr::drop_na(value) %>%
   mutate(symbol = case_when(
@@ -25,12 +26,9 @@ clean_df <- tidyr::pivot_longer(raw_df, cols = c(Q101, Q103, Q107, Q98),
   teacher = clean_var(value, target = "teacher"),
   year = 2022,
   weight = NA) %>%
-  select(year, symbol, authority, teacher, weight)
+  select(id_respondent, symbol, authority, teacher)
 
-saveRDS(clean_df, "SignesReligieux2024/Data/cleandata/by_year/data2022.rds")
-
-
-# Cleaning ----------------------------------------------------------------
+# Weights ----------------------------------------------------------------
 
 raw_df_weights <- raw_df %>%
   mutate(age = case_when(
@@ -49,18 +47,41 @@ raw_df_weights <- raw_df %>%
     language_weight == "EN" ~ "en"),
   langue = ifelse(!(language_weight %in% c("FR", "EN")), "other", langue)
   ) %>%
-  select(id_respondent, age, gender, langue)
-
-# Pondération -------------------------------------------------------------
+  select(id_respondent, age, gender, langue) %>%
+  tidyr::drop_na() %>%
+  fastDummies::dummy_cols(select_columns = c("age", "gender", "langue"),
+                          remove_selected_columns = TRUE)
 
 marginals <- c(`(Intercept)` = 1,
-               age18_29 = 0.1689,
-               age30_39 = 0.1586,
-               age40_49 = 0.1590,
-               age50_64 = 0.2575,
-               age65_99 = 0.2560,
-               genderfemale = 0.5065,
-               gendermale = 0.4935,
-               languefr = 0.8219,
-               langueen = 0.1295,
-               langueother = 0.0486)
+               age_18_29 = 0.1689,
+               age_30_39 = 0.1586,
+               age_40_49 = 0.1590,
+               age_50_64 = 0.2575,
+               age_65_99 = 0.2560,
+               gender_female = 0.5065,
+               gender_male = 0.4935,
+               langue_fr = 0.8219,
+               langue_en = 0.1295,
+               langue_other = 0.0486)
+tmp_form <- paste(" ~ 1 +", paste(names(marginals)[names(marginals) != "(Intercept)"],
+                                  collapse=" + "), sep = "")
+
+surveyDesign <- survey::svydesign(id = ~ 1, #no clustering
+                                  weights = ~ 1,
+                                  data = raw_df_weights)
+
+surveyDesign <- survey::calibrate(design     = surveyDesign,
+                                  formula    = as.formula(tmp_form),
+                                  calfun     = "raking",
+                                  population = marginals,
+                                  #eps        = 0.005,
+                                  maxit      = 20000)
+
+weights <- setNames(weights(surveyDesign), 1:length(weights(surveyDesign)))
+
+clean_df <- df_symbols %>%
+  mutate(weight = weights[id_respondent],
+         year = 2018) %>%
+  select(year, symbol, authority, teacher, weight)
+
+saveRDS(clean_df, "SignesReligieux2024/Data/cleandata/by_year/data2022.rds")
