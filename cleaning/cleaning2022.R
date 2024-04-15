9b5a9b44645dd94843506c9fb575c9a90aaba0fb
sarahjanevincent <141277350+sarahjanevincent@users.noreply.github.com>
2024-04-15 15:34:12 -0400

    pondération 2022

--- cleaning/cleaning2022.R
+++ cleaning/cleaning2022.R
@@ -5,8 +5,10 @@ source("functions.R")
 # Data --------------------------------------------------------------------
 
 raw_df <- read.csv("SignesReligieux2024/Data/pes_qc22_relig_symb/2022-11-09_pes_qc22_religious_symb_data.csv") %>%
-  select(Q101, Q103, Q107, Q98)
-
+  select(Q101, Q103, Q107, Q98,
+         ## for weighting
+         age_weight, gender_weight, language_weight) %>%
+  mutate(id_respondent = 1:nrow(.))
 
 # Clean -------------------------------------------------------------------
 
@@ -28,14 +30,35 @@ clean_df <- tidyr::pivot_longer(raw_df, cols = c(Q101, Q103, Q107, Q98),
 saveRDS(clean_df, "SignesReligieux2024/Data/cleandata/by_year/data2022.rds")
 
 
+# Cleaning ----------------------------------------------------------------
+
+raw_df_weights <- raw_df %>%
+  mutate(age = case_when(
+     age_weight == "18-29" ~ "18_29",
+     age_weight == "30-39" ~ "30_39",
+     age_weight == "40-49" ~ "40_49",
+     age_weight == "50-64" ~ "50_64",
+     age_weight == "65+" ~ "65_99"
+  ),
+  gender = case_when(
+    gender_weight  %in% c("Female", "Woman", "Women") ~ "female",
+    gender_weight  %in% c("Male", "Masculin", "Man", "Men") ~ "male"
+  ),
+  langue = case_when(
+    language_weight == "FR" ~ "fr",
+    language_weight == "EN" ~ "en"),
+  langue = ifelse(!(language_weight %in% c("FR", "EN")), "other", langue)
+  ) %>%
+  select(id_respondent, age, gender, langue)
+
 # Pondération -------------------------------------------------------------
 
 marginals <- c(`(Intercept)` = 1,
-               age18_34 = 0.2472,
-               age35_44 = 0.1639,
-               age45_59 = 0.2416,
-               age60_74 = 0.2370,
-               age75_99 = 01103,
+               age18_29 = 0.1689,
+               age30_39 = 0.1586,
+               age40_49 = 0.1590,
+               age50_64 = 0.2575,
+               age65_99 = 0.2560,
                genderfemale = 0.5065,
                gendermale = 0.4935,
                languefr = 0.8219,
