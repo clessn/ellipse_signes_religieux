721871f3c82c17e72a2c1cf6c14efb839a47e49c
hubcad25 <hubertcadieux@gmail.com>
2024-05-21 11:28:26 -0400

    add all signs 2015

--- cleaning/cleaning2015.R
+++ cleaning/cleaning2015.R
@@ -1,31 +1,30 @@
 # Packages ----------------------------------------------------------------
 library(dplyr)
-
-## Fonction pour nettoyer la variable (utilisée pour chaque sondage)
-clean_var <- function(raw_vector, target = c("teacher", "authority")){
-  patterns <- c("teacher" = "public school teachers",
-                "authority" = "government officials")
-  pattern <- patterns[target]
-  output <- as.numeric(grepl(pattern = pattern, x = raw_vector))
-  return(output)
-}
+source("functions.R")
 
 # Data --------------------------------------------------------------------
 
 raw_df <- read.csv("SignesReligieux2024/Data/data_pes_canada2015_weights.csv") %>%
   ### filter for Quebec
   filter(X1.2 == "Quebec") %>%
-  select(X, X16.5, X16.7, X16.11, X16.2, weight)
+  select(X, all_of(paste0("X16.", 2:12)), weight)
 
 # Clean -------------------------------------------------------------------
-clean_df <- tidyr::pivot_longer(raw_df, cols = c(X16.5, X16.7, X16.11, X16.2),
+clean_df <- tidyr::pivot_longer(raw_df, cols = starts_with("X16"),
                                 names_to = "symbol", values_to = "value") %>%
   tidyr::drop_na(value) %>%
   mutate(symbol = case_when(
+    symbol == "X16.2" ~ "grosse_croix",
+    symbol == "X16.3" ~ "burqa",
+    symbol == "X16.4" ~ "chador",
     symbol == "X16.5" ~ "hijab",
+    symbol == "X16.6" ~ "david",
     symbol == "X16.7" ~ "pendantif_croissant",
+    symbol == "X16.8" ~ "kippa",
+    symbol == "X16.9" ~ "kirpan",
+    symbol == "X16.10" ~ "niqab",
     symbol == "X16.11" ~ "petite_croix",
-    symbol == "X16.2" ~ "grosse_croix"
+    symbol == "X16.12" ~ "turban"
   ),
   authority = clean_var(value, target = "authority"),
   teacher = clean_var(value, target = "teacher"),
