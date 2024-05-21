f812f136539adfe15d03d378af8a2c472a873b9a
hubcad25 <hubertcadieux@gmail.com>
2024-05-21 11:39:25 -0400

    change function to capture all cases

--- functions.R
+++ functions.R
@@ -1,6 +1,10 @@
-clean_var <- function(raw_vector, target = c("teacher", "authority")){
+clean_var <- function(raw_vector, target = c("teacher", "authority", "citizenship",
+                                             "students", "all_public")){
   patterns <- c("teacher" = "public school teachers",
-                "authority" = "government officials")
+                "authority" = "government officials",
+                "citizenship" = "citizenship ceremony",
+                "students" = "students",
+                "all_public" = "public spaces")
   pattern <- patterns[target]
   output <- as.numeric(grepl(pattern = pattern, x = raw_vector))
   return(output)
