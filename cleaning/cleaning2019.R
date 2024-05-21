37f84d0f647102f8182631eb296fe1a3d18e552a
sarahjanevincent <141277350+sarahjanevincent@users.noreply.github.com>
2024-05-21 11:34:16 -0400

    Clean 2019

--- cleaning/cleaning2019.R
+++ cleaning/cleaning2019.R
@@ -6,18 +6,25 @@ source("functions.R")
 
 raw_df <- read.csv("SignesReligieux2024/Data/data_pes_canada2019_weights.csv") %>%
   filter(Q3 == "Quebec" | Q4 == "Quebec") %>%
-  select(Q190, Q192, Q196, Q187, weight)
+  select(Q187, Q188, Q189, Q190, Q191, Q192, Q193, Q194, Q195, Q196, Q197, weight)
 
 # Clean -------------------------------------------------------------------
-clean_df <- tidyr::pivot_longer(raw_df, cols = c(Q190, Q192, Q196, Q187),
+clean_df <- tidyr::pivot_longer(raw_df, cols = c(Q187, Q188, Q189, Q190, Q191, Q192, Q193, Q194, Q195, Q196, Q197),
                                 names_to = "symbol", values_to = "value") %>%
 
   tidyr::drop_na(value) %>%
   mutate(symbol = case_when(
+    symbol == "Q187" ~ "grosse_croix",
+    symbol == "Q188" ~ "burqa",
+    symbol == "Q189" ~ "chador",
     symbol == "Q190" ~ "hijab",
+    symbol == "Q191" ~ "david",
     symbol == "Q192" ~ "pendantif_croissant",
+    symbol == "Q193" ~ "kippa",
+    symbol == "Q194" ~ "kirpan",
+    symbol == "Q195" ~ "niqab",
     symbol == "Q196" ~ "petite_croix",
-    symbol == "Q187" ~ "grosse_croix"
+    symbol == "Q197" ~ "turban",
   ),
   authority = clean_var(value, target = "authority"),
   teacher = clean_var(value, target = "teacher"),
